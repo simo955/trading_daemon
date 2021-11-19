477aef06debfd7ad1884cfccdf5d66b6c3caa797
simo955 <s.patu9555@gmail.com>
2021-11-19 16:09:55 +0100

    Added working /help handler

--- indexBot.py
+++ indexBot.py
@@ -3,10 +3,11 @@ import time
 
 from telegram import Update
 from telegram.ext import Updater, MessageHandler, Filters,CommandHandler, CallbackContext
+from telegram.utils.helpers import escape_markdown
 
 from getQuotes import manage_stack
 from utils import areBotConfigurationsValids
-from conf import STARTING_SYMBOL, SLEEP_SECONDS, BOT_NAME,UPDATE_MSG, bot_configuration_cmd, bot_start_deamong_cmd
+from conf import STARTING_SYMBOL, SLEEP_SECONDS, BOT_NAME,UPDATE_MSG,WIKI_URL, bot_configuration_cmd, bot_start_deamong_cmd
 from keys import BOT_TOKEN
 
 # Enable logging
@@ -16,16 +17,19 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update: Update, _: CallbackContext) -> None:
-    """Sends explanation on how to use the bot."""
+def startCmd(update: Update, _: CallbackContext) -> None:
     update.message.reply_text(
         'Hi welcom to {}! First: configure your deamon using: {} <TICKER> <POLLING_SECONDS>'.format(BOT_NAME,bot_configuration_cmd),
          quote=True
          )
 
-def start_deamon(update: Update, _: CallbackContext) -> None:
+def helpCmd(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text(
+        'Want to know more about Trading-Deamon? Click <a href="{}">here</a>'.format(WIKI_URL),
+        parse_mode='HTML'
+        )
+
+def start_deamonCmd(update: Update, _: CallbackContext) -> None:
     global isConfigured
     if not isConfigured:
         update.message.reply_text(
@@ -35,7 +39,7 @@ def start_deamon(update: Update, _: CallbackContext) -> None:
         return 
     quotes_list=[]
     update.message.reply_text('Daemon is starting')
-    while True:
+    for i in range(0,5):
         logger.debug('Still running')
         msg = manage_stack(logger, quotes_list, STARTING_SYMBOL)
         logger.info('Ending iteration. MSG={}'.format(msg))
@@ -44,7 +48,7 @@ def start_deamon(update: Update, _: CallbackContext) -> None:
         time.sleep(SLEEP_SECONDS)
 
 #Function used to to set all the configurations required for running the deamon
-def configure_bot(update: Update, context: CallbackContext) -> None:
+def configure_botCmd(update: Update, context: CallbackContext) -> None:
     global isConfigured
     global STARTING_SYMBOL
     global SLEEP_SECONDS
@@ -75,10 +79,10 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", start))
-    dispatcher.add_handler(CommandHandler(bot_configuration_cmd, configure_bot))
-    dispatcher.add_handler(CommandHandler(bot_start_deamong_cmd, start_deamon))
+    dispatcher.add_handler(CommandHandler("start", startCmd))
+    dispatcher.add_handler(CommandHandler("help", helpCmd))
+    dispatcher.add_handler(CommandHandler(bot_configuration_cmd, configure_botCmd))
+    dispatcher.add_handler(CommandHandler(bot_start_deamong_cmd, start_deamonCmd))
 
     # on non command i.e message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echoWrongCommand))
